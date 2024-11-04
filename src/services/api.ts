9f873bd3e211f49070895824e1b2d321f2185e4f
Ganiev Rustam <ganiev.rystam@gmail.com>
2024-11-04 20:35:08 +0300

    design

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2';
 
-export const fetchNews = async (category: string) => {
+export const fetchNews = async (category: string, page: number) => {
   try {
     const response = await axios.get(`${BASE_URL}/top-headlines`, {
       params: {
